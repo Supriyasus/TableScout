3e07ae6fa6271d9a4d6eea817088cfd16fa3725e
Pratyush Dube <pratyushdube23@gmail.com>
2026-01-08 12:16:52 +0530

    Working Frontend

--- backend/api/v1/user.py
+++ backend/api/v1/user.py
@@ -4,4 +4,4 @@ router = APIRouter(prefix="/user", tags=["User"])
 
 @router.get("/preferences")
 def get_user_preferences():
-    return {"message": "User API working"}
+    return {"preferences": ["quiet", "wifi", "low budget"]}
